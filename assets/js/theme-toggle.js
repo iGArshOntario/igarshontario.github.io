4c76e1a345e27b53f940c0cfdc43c483a88dbcfd
iGArshOntario <strongspoon.ca@gmail.com>
2025-05-22 11:20:35 -0400

    Delete assets directory

--- assets/js/theme-toggle.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('theme-toggle').addEventListener('click', () => {
-  const html = document.documentElement;
-  html.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
-});
